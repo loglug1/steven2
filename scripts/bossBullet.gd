12490bee9cb0439b96a450c264f969fa1c8934f2
loglug1 <62907417+loglug1@users.noreply.github.com>
2023-10-14 01:47:32 -0500

    implemented simple boss ai targeting player

--- scripts/bossBullet.gd
+++ scripts/bossBullet.gd
@@ -1,17 +1,22 @@
 extends Node2D
 
-var speed = 2000
-var target
+var speed = 1500
 var direction
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	target = get_global_mouse_position()
+	#goes towards the player to start
+	var player_node = get_node("/root/gamescene/CharacterBody2D")
+	var target
+	if player_node:
+		target = player_node.position
+	else:
+		target = get_global_mouse_position()
 	direction = global_position.direction_to(target)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 
@@ -20,7 +25,7 @@ func _physics_process(delta):
 
 
 func _on_body_entered(body):
-	#send signal to player health
+	#damage player
 	var progress_bar = body.find_child("ProgressBar")
 	if progress_bar:
 		progress_bar.decrease_value(50)
